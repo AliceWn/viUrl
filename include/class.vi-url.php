8af1f29ad41e68f7e6b4ff271005387f2c325588
radoslav-vitanov <radoslav@safecharge.com>
2014-09-16 12:21:48 +0300

    Add some more methods

--- include/class.vi-url.php
+++ include/class.vi-url.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ # Copyright (C) 2014 Radoslav Vitanov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 namespace Vi\Url;
 
@@ -9,9 +27,14 @@ namespace Vi\Url;
  */
 class viUrl {
 	
-	const URL_SCHEME_SEPARATOR = '://';
-	const URL_PORT_SEPARATOR = ':';
-	const URL_PATH_SEPARATOR = '/';
+	const URL_SCHEME_SEPARATOR 		= '://';
+	const URL_PORT_SEPARATOR 		= ':';
+	const URL_PATH_SEPARATOR 		= '/';
+	const URL_FRAGMENT_SEPARATOR 	= '#';
+	const URL_QUERY_GLUE			= '?';
+	const URL_QUERY_SEPARATOR		= '&';
+	const URL_QUERY_PAIR_GLUE		= '=';
+	const URL_AUTH_SEPARATOR		= '@';
 	
 	/**
 	 * Url scheme
@@ -61,6 +84,84 @@ class viUrl {
 	 * @var int
 	 */
 	protected $_currentPathIndex = null;
+	
+	/**
+	 * Holds url frament
+	 * @var string
+	 */
+	protected $_fragment = null;
+	
+	/**
+	 * Holds array with query parameters for the url
+	 * @var array
+	 */
+	protected $_queryParams = array();
+	
+	/**
+	 * Flag to force www on domain
+	 * @var boolean
+	 */
+	protected $_forceWWW = false;
+	
+	/**
+	 * Class constructor
+	 * @param string $url
+	 */
+	public function __construct($url = '') {
+		if (!empty($url)) {
+			$parts = parse_url($url);
+			
+			if (isset($parts['scheme'])) {
+				$this->setScheme($parts['scheme']);
+			}
+			
+			if (isset($parts['host'])) {
+				$this->setDomain($parts['host']);
+			}
+			
+			if (isset($parts['user'])) {
+				$this->setUsername($parts['user']);
+			}
+			
+			if (isset($parts['pass'])) {
+				$this->setPassword($parts['pass']);
+			}
+			
+			if (isset($parts['path'])) {
+				$this->setPath($parts['path']);
+			}
+			
+			if (isset($parts['query'])) {
+				$this->setQueryParams($parts['query']);
+			}
+			
+			if (isset($parts['fragment'])) {
+				$this->setFragment($parts['fragment']);
+			}
+			
+			if (isset($parts['port'])) {
+				$this->setPort($parts['port']);
+			}
+		}
+	}
+	
+	/**
+	 * Force domain to be www
+	 * @param boolean $boolean
+	 * @return \Vi\Url\viUrl
+	 */
+	public function setForceWWW($boolean) {
+		$this->_forceWWW = $boolean;
+		return $this;
+	}
+	
+	/**
+	 * Get force www url flag
+	 * @return boolean
+	 */
+	public function getForceWWW() {
+		return $this->_forceWWW;
+	}
 	
 	/**
 	 * Set scheme for the Url
@@ -131,7 +232,11 @@ class viUrl {
 	 * @return string
 	 */
 	public function getDomain() {
-		return $this->_domain;
+		$prefix = '';
+		if ($this->getForceWWW() && substr($this->_domain, 0, 3) != 'www') {
+			$prefix = 'www.';
+		}
+		return $prefix . $this->_domain;
 	}
 	
 	/**
@@ -153,6 +258,94 @@ class viUrl {
 	}
 	
 	/**
+	 * Set query params for this url
+	 * @param array $params
+	 * @return \Vi\Url\viUrl
+	 */
+	public function setQueryParams($params) {
+		if (!is_array($params)) {
+			$tmp = explode(self::URL_QUERY_SEPARATOR, $params);
+			
+			$newParams = array();
+			foreach ($tmp as $plain) {
+				$parts = explode(self::URL_QUERY_PAIR_GLUE, $plain);
+				$newParams[$parts[0]] = $parts[1];
+			}
+			$params = $newParams;
+		}
+		
+		if (!is_array($params)) {
+			throw new \viUrlException("Invalid parameter passed to viUrl::setQueryParams. Expecting array.");
+		}
+		
+		$this->_queryParams = $params;
+		return $this;
+	}
+	
+	/**
+	 * Get query params for this url
+	 * @return array
+	 */
+	public function getQueryParams() {
+		return $this->_queryParams;
+	}
+	
+	/**
+	 * Get query param
+	 * @param unknown $paramName
+	 * @return Ambigous <NULL, multitype:>
+	 */
+	public function getQueryParam($paramName) {
+		return (isset($this->_queryParams[$paramName]) ? $this->_queryParams[$paramName] : null);
+	}
+	
+	/**
+	 * Add query parameter
+	 * @param string $paramName
+	 * @param string $paramValue
+	 * @param boolean $overwrite
+	 * @return \Vi\Url\viUrl
+	 */
+	public function setQueryParam($paramName, $paramValue, $overwrite = true) {
+		if (!isset($this->_queryParams[$paramName]) || $overwrite) {
+			$this->_queryParams[$paramName] = $paramValue;
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Unset query parameter
+	 * @param string $paramName
+	 * @return \Vi\Url\viUrl
+	 */
+	public function removeQueryParam($paramName) {
+		if (isset($this->_queryParams[$paramName])) {
+			unset($this->_queryParams[$paramName]);
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Set fragment for this url
+	 * @param string $fragment
+	 * @return \Vi\Url\viUrl
+	 */
+	public function setFragment($fragment) {
+		$this->_fragment = $fragment;
+		return $this;
+	}
+	
+	/**
+	 * Get url fragment
+	 * @return string
+	 */
+	public function getFragment() {
+		return $this->_fragment;
+	}
+	
+	/**
 	 * Set path for this Url
 	 * @param array|string $path
 	 * @return \Vi\Url\viUrl
@@ -160,6 +353,7 @@ class viUrl {
 	public function setPath($path) {
 		// Make sure to always assign array. Convert strings like /my/web/path to array.
 		if (!is_array($path)) {
+			$path = trim($path, self::URL_PATH_SEPARATOR);
 			$path = explode(self::URL_PATH_SEPARATOR, $path);
 		}
 		
@@ -189,7 +383,7 @@ class viUrl {
 		$currentPathIndex = null;
 		
 		foreach ($this->_path as $pathIndex => $pathName) {
-			if ($this->_stringsAreSame($pathToFind, $pathName, $caseSentitive)) {
+			if ($this->_stringsAreSame($pathToFind, $pathName, $caseSensitive)) {
 				break;
 			}
 		}
@@ -208,7 +402,97 @@ class viUrl {
 		return (($caseSentitive) ? ($nameOne == $nameTwo) : (strtolower($nameOne) == strtolower($nameTwo)));
 	}
 	
+	/**
+	 * Replace path fragment with another
+	 * @param string $pathToFind
+	 * @param string $pathToReplace
+	 * @return \Vi\Url\viUrl
+	 */
 	public function replacePath($pathToFind, $pathToReplace) {
 		$this->findPath($pathToFind);
+		
+		if (isset($this->_path[$this->_currentPathIndex])) {
+			$this->_path[$this->_currentPathIndex] = $pathToReplace;
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Inserts path fragment before another fragment
+	 * @param string $fragmentToFind
+	 * @param string $fragmentToInsert
+	 * @return \Vi\Url\viUrl
+	 */
+	public function insertPathBefore($fragmentToFind, $fragmentToInsert) {
+		$this->findPath($fragmentToFind);
+		
+		$firstPart = array_slice($this->_path, 0, $this->_currentPathIndex);
+		$secondPart = array_slice($this->_path, $this->_currentPathIndex);
+		
+		$this->_path = array_merge($firstPart, array($fragmentToInsert), $secondPart);
+		return $this;
+	}
+	
+	/**
+	 * Insert fragment after another path fragment
+	 * @param string $fragmentToFind
+	 * @param string $fragmentToInsert
+	 * @return \Vi\Url\viUrl
+	 */
+	public function insertPathAfter($fragmentToFind, $fragmentToInsert) {
+		$this->findPath($fragmentToFind);
+	
+		$firstPart = array_slice($this->_path, 0, ($this->_currentPathIndex + 1));
+		$secondPart = array_slice($this->_path, ($this->_currentPathIndex + 1));
+	
+		$this->_path = array_merge($firstPart, array($fragmentToInsert), $secondPart);
+		return $this;
+	}
+	
+	/**
+	 * Delete path
+	 * @param string $pathName
+	 * @return \Vi\Url\viUrl
+	 */
+	public function deltePath($pathName) {
+		$this->findPath($pathName);
+		if (isset($this->_path[$this->_currentPathIndex])) {
+			unset($this->_path[$this->_currentPathIndex]);
+		}
+		return $this;
+	}
+	
+	/**
+	 * Builds an Url
+	 * @return string
+	 */
+	public function buildUrl() {
+		$url = '';
+		
+		$url .= $this->getScheme() . self::URL_SCHEME_SEPARATOR;
+		
+		if (!empty($this->_userName) && !empty($this->_password)) {
+			$url .= $this->getUsername() . self::URL_PORT_SEPARATOR . $this->getPassword() . self::URL_AUTH_SEPARATOR;
+		}
+		
+		$url .= $this->getDomain();
+		
+		if (!empty($this->_port)) {
+			$url .= self::URL_PORT_SEPARATOR . $this->getPort();
+		}
+		
+		if (!empty($this->_path)) {
+			$url .= self::URL_PATH_SEPARATOR . join(self::URL_PATH_SEPARATOR, $this->getPath());
+		}
+		
+		if (!empty($this->_queryParams)) {
+			$url .= self::URL_QUERY_GLUE . http_build_query($this->_queryParams);
+		}
+		
+		if (!empty($this->_fragment)) {
+			$url .= self::URL_FRAGMENT_SEPARATOR . $this->getFragment();
+		}
+		return $url;
 	}
 }
--- tests/test.php
+++ tests/test.php
@@ -1,1 +1,60 @@
 <?php
+/*
+# Copyright (C) 2014 Radoslav Vitanov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+# software and associated documentation files (the "Software"), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, merge, 
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or 
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+use Vi\Url\viUrl;
+require_once '../include/class.vi-url-exception.php';
+require_once '../include/class.vi-url.php';
+
+$url = new viUrl('http://test.example.com/test1/test2');
+
+// This will output - http://test.example.com/test1/test2
+echo $url->buildUrl();
+
+// Let's force WWW - http://www.test.example.com/test1/test2
+echo $url->setForceWWW(true)->buildUrl();
+
+// You can change scheme - https://www.test.example.com/test1/test2
+echo $url->setScheme('https')->buildUrl();
+
+// You can append and prepend parts to the path - https://www.test.example.com/test1/test3/test2
+echo $url->insertPathAfter('test1', 'test3')->buildUrl();
+
+// https://www.test.example.com/test1/test4/test3/test2
+echo $url->insertPathBefore('test3', 'test4')->buildUrl();
+
+// You can also replace or delete paths - https://www.test.example.com/test1/test4/test3/new
+echo $url->replacePath('test2', 'new')->buildUrl();
+
+// https://www.test.example.com/test4/test3/new
+echo $url->deltePath('test1')->buildUrl();
+
+// You can also assign query parameters - https://www.test.example.com/test4/test3/new?key1=value1&key2=value2
+echo $url->setQueryParams(array('key1' => 'value1', 'key2' => 'value2'))->buildUrl();
+
+// You can manipulate query parameters aswell - https://www.test.example.com/test4/test3/new?key1=testValue&key2=value2
+echo $url->setQueryParam('key1', 'testValue')->buildUrl();
+
+// https://www.test.example.com/test4/test3/new?key1=testValue
+echo $url->removeQueryParam('key2')->buildUrl();
+
+// You can also add fragment to the url - https://www.test.example.com/test4/test3/new?key1=testValue#randomHash
+echo $url->setFragment('randomHash')->buildUrl();
+
+// Add a port - https://www.test.example.com:8888/test4/test3/new?key1=testValue#randomHash
+echo $url->setPort(8888)->buildUrl();
